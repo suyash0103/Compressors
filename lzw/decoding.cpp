97750eec65fc6dcbaa65ffadc0357675ef3dcccd
SanjayShetty <sanjayshetty2015@gmail.com>
2019-03-19 19:35:28 +0530

    12 bit I/O

--- lzw/decoding.cpp
+++ lzw/decoding.cpp
@@ -2,10 +2,11 @@
 #include <fstream>
 #include <vector>
 #include <string>
+
 using namespace std;
 
-void lzwDecode(ifstream& i,ostream& o,ifstream& codefile){
-    vector<string> codebook;
+void lzwDecode(ifstream &i, ostream &o, ifstream &codefile) {
+    vector <string> codebook;
     for (int i = 0; i < 256; i++) {
         codebook.push_back(std::string(1, i) = i);
     }
@@ -14,27 +15,33 @@ void lzwDecode(ifstream& i,ostream& o,ifstream& codefile){
     string present;
     while (!codefile.eof()) {
         codefile.get(c);
-        if (c==0) {
+        if (c == 0) {
             codebook.push_back(present);
             present.clear();
         } else {
-            present=present+c;
+            present = present + c;
         }
     }
 
-    int toRestore=0;
-    while(!i.eof()){
-        toRestore=0;
-        i.read((char*)&toRestore,sizeof(toRestore));
-        o << codebook[toRestore];
+    int toRestore = 0;
+    while (!i.eof()) {
+        toRestore = 0;
+        i.read((char *) &toRestore, 3);
+        if (toRestore >> 12)
+            o << codebook[toRestore >> 12];
+        int hex = 0xFFF;
+        toRestore = (toRestore & hex);
+        if (toRestore)
+            o << codebook[toRestore];
     }
 }
 
-int main (int argc, char* argv[]){
-    ifstream i(argv[1],ios::binary);
-    ofstream o(argv[2],ios::out|ios::trunc);
-    ifstream codefile(argv[3],ios::in);
-    lzwDecode(i,o,codefile);
+int main(int argc, char *argv[]) {
+    ifstream i(argv[1], ios::binary);
+    ofstream o(argv[2], ios::out | ios::trunc);
+    ifstream codefile(argv[3], ios::in);
+    lzwDecode(i, o, codefile);
+    cout << "DecodedToFile:" << argv[2] << endl;
     o.close();
     i.close();
     codefile.close();
--- lzw/encoding.cpp
+++ lzw/encoding.cpp
@@ -7,11 +7,17 @@ using namespace std;
 
 struct binary_buffer {
     int buffer = 0;
-    int n = 32; //number of bits remianing
 };
 
 void write_buffer(binary_buffer &buff, ofstream &o, int a, int req = 12) {
-    o.write((char *) &a, sizeof(int));
+    if (buff.buffer == 0) {
+        buff.buffer = a;
+    } else {
+        buff.buffer = buff.buffer << req;
+        a = a | buff.buffer;
+        o.write((char *) &a, 3);
+        buff.buffer = 0;
+    }
 }
 
 void lzwEncode(char ipfile[], char opfile[], char codefile[]) {
@@ -28,10 +34,10 @@ void lzwEncode(char ipfile[], char opfile[], char codefile[]) {
         encodetable[std::string(1, i)] = i;
     }
 
+    //encoding
     char p, c;
     int noofcharinput = 0, noofcharoutput = 0;
     binary_buffer buf;
-
     if (!fin.eof()) fin.get(p);
     string present;
     present = p;
@@ -43,31 +49,34 @@ void lzwEncode(char ipfile[], char opfile[], char codefile[]) {
         } else if (map_current < 4096) {
             noofcharoutput++;
             map_current++;
-//            cout<<present<<" "<<encodetable[present]<<endl; //output
+            //cout<<present<<" "<<encodetable[present]<<endl; //output
             write_buffer(buf, o, encodetable[present]);
             encodetable[present + c] = map_current;
             decodetable.push_back(present + c);
             present = c;
         } else {
             noofcharoutput++;
-//            cout<<present<<" "<<encodetable[present]<<endl; //output
+            //cout<<present<<" "<<encodetable[present]<<endl; //output
             write_buffer(buf, o, encodetable[present]);
             present = c;
         }
     }
-    fin.close();
-    o.close();
-    int size=0;
-    char null=0;
+    if (buf.buffer != 0)write_buffer(buf, o, 0);
+
+    //writing codebook to file
+    int size = 0;
+    char null = 0;
     ofstream codebook(codefile, ios::out);
     for (int i = 256; i < decodetable.size(); i++) {
-        codebook<<decodetable[i]<<null;
-        size=size+decodetable[i].length();
+        codebook << decodetable[i] << null;
+        size = size + decodetable[i].length();
     }
-    cout<<"code book size"<<size<<endl;
-    codebook.close();
+    cout << "codeBookSize:" << size << endl;
     cout << "noofcharinput:" << noofcharinput << endl;
     cout << "noofcharoutput:" << noofcharoutput << endl;
+    codebook.close();
+    fin.close();
+    o.close();
 }
 
 int main(int argc, char *argv[]) {
