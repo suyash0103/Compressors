52ac7738ccc0ee62af55f0020f5936aa0191bc4a
SanjayShetty <sanjayshetty2015@gmail.com>
2019-04-09 19:30:28 +0530

    type 2 encoding using 8 bit flag

---
+++ LocalMap/decoding-type2.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <ctime>
+#include <chrono>
+
+using namespace std::chrono;
+using namespace std;
+
+/**
+ * @brief Read data from buffer.
+ * buff_read @author srinskit
+ * Note that "code" should be read only when function returns 1. When 0 is
+ * returned, call the function again with next buffer to complete reading of
+ * "code".
+ *
+ * @param buff The pointer to the buffer to be read from.
+ * @param code The pointer to the variable receiving the data.
+ * @param code_len The length of data to be read in bits.
+ * @param flush Set to 1 to reset function.
+ * @return int -1 if error, 1 if success, 0 if function needs to be recalled
+ * with next buffer.
+ */
+int buff_read(uint64_t *buff, uint64_t *code, unsigned code_len, int flush) {
+    const unsigned buff_len = 8 * sizeof(*buff);
+    static unsigned filled_len = buff_len;
+    static unsigned to_read_len = 0;
+    if (code_len > buff_len) return -1;
+    if (flush) {
+        filled_len = buff_len;
+        to_read_len = 0;
+        return 1;
+    }
+    if (to_read_len != 0) {
+        *code <<= to_read_len;
+        *code += *buff >> (buff_len - to_read_len);
+        *buff <<= to_read_len;
+        filled_len -= to_read_len;
+        to_read_len = 0;
+        return 1;
+    }
+    if (code_len <= filled_len) {
+        *code = *buff >> (buff_len - code_len);
+        *buff <<= code_len;
+        filled_len -= code_len;
+        return 1;
+    }
+    to_read_len = code_len - filled_len;
+    *code = *buff >> (buff_len - filled_len);
+    filled_len = buff_len;
+    return 0;
+}
+
+void Decode(char ipfile[], char opfile[], char dictionary[]) {
+    ifstream fin(ipfile, ios::binary);
+    ofstream o(opfile, ios::binary );
+    ifstream codefile(dictionary, ios::in);
+
+    vector<string> table;
+    string present;
+    int i = 0;
+    while (!codefile.eof()) {
+        codefile >> present;
+        table.push_back(present);
+        i++;
+    }
+
+    string s;
+    uint64_t buff = 0,code;
+    if (!fin.read((char *) &buff, sizeof(buff))) return;
+    while(1){
+        code=0;  //read first bit and find if its 19 bit or 9 bit encoding
+        if (buff_read(&buff, &code,8, 0) == 0) {
+            if (!fin.read((char *) &buff, sizeof(buff))) break;
+            buff_read(&buff, &code,8, 0);
+        }
+        if(code==-125){
+            code=0;
+            if (buff_read(&buff, &code,18, 0) == 0) {
+                if (!fin.read((char *) &buff, sizeof(buff))) break;
+                buff_read(&buff, &code,18, 0);
+            }
+
+            if(code==0){break;}
+            if(code & (1<<17)){
+                //cout<<code<<":"<<(code ^ (1<<17))<<endl;
+                code=code ^ (1<<17);
+                s=table[code];
+                s[0]=toupper(s[0]);
+                o<<s;
+                //cout<<"\n"<<s<<" "<<code<<"\n";
+            }else{
+                s=table[code];
+                o<<s;
+                //cout<<"\n"<<s<<" "<<code<<"\n";
+            }
+        }else{
+            code=0;
+            if (buff_read(&buff, &code,8, 0) == 0) {
+                if (!fin.read((char *) &buff, sizeof(buff))) break;
+                buff_read(&buff, &code,8, 0);
+            }
+            if(code==0){break;}
+            o<<(char)code;
+            //cout<<(char)code<<":"<<code<<endl;
+        }
+    }
+    o.close();
+    fin.close();
+    codefile.close();
+}
+
+int main(int argc, char *argv[]) {
+    if(argc!=4){
+        cout<<"Correct usage: ./a.out ipfile opfile codebook";
+        exit(0);
+    }
+
+    auto start = high_resolution_clock::now();
+    Decode(argv[1],argv[2],argv[3]);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by function: "<< duration.count() << " microseconds" << endl;
+    return 0;
+}
---
+++ LocalMap/encoding-type2.cpp
@@ -0,0 +1,161 @@
+#include<iostream>
+#include<fstream>
+#include<unordered_map>
+#include<string>
+#include<ctime>
+#include <chrono>
+
+using namespace std::chrono;
+using namespace std;
+
+/**
+ * @brief Write data into buffer.
+ * buff_read @author srinskit
+ * Note that when function returns 0, the function has to be recalled with new
+ * buffer.
+ *
+ * @param buff The pointer to the buffer to be written to.
+ * @param code The data to be written.
+ * @param code_len The length of data to be written in bits.
+ * @param flush Set to 1 to flush buffer and reset function.
+ * @return int -1 if error, 1 if success, 0 if function needs to be recalled
+ * with next buffer.
+ * @return int 1 if flush didn't modify buffer, 0 if flush modified buffer.
+ */
+int buff_write(uint64_t *buff, uint64_t code, unsigned code_len, int flush) {
+    const unsigned buff_len = 8 * sizeof(*buff);
+    static unsigned vacant_len = buff_len;
+    static unsigned to_write_len = 0;
+    if (code_len > buff_len) return -1;
+    if (flush) {
+        if (vacant_len == buff_len) return 0;
+        *buff <<= vacant_len;
+        int ret = buff_len - vacant_len;
+        vacant_len = buff_len;
+        to_write_len = 0;
+        return ret;
+    }
+    if (to_write_len != 0) {
+        *buff = code - ((code >> to_write_len) << to_write_len);
+        vacant_len -= to_write_len;
+        to_write_len = 0;
+        return 1;
+    }
+    if (code_len <= vacant_len) {
+        *buff <<= code_len;
+        *buff += code;
+        vacant_len -= code_len;
+        return 1;
+    }
+    to_write_len = code_len - vacant_len;
+    *buff <<= vacant_len;
+    *buff += code >> to_write_len;
+    vacant_len = buff_len;
+    return 0;
+}
+
+
+void prepare_buffer(uint64_t &buff, ofstream &o, int code_32, int bit_length, short int capital = 0) {
+    uint64_t code=code_32;
+    unsigned char delimiter=-125;
+    if (bit_length == 18) {
+        //for 18 bit code 1st bit is 1
+        if (capital) {
+            code = code | (1 << 17);
+        }
+        //writing to file
+        if (buff_write(&buff, delimiter, 8, 0) == 0) {
+            o.write((char *) &buff, sizeof(buff));
+            buff = 0;
+            buff_write(&buff, delimiter, 8, 0);
+        }
+        if (buff_write(&buff, code, 18, 0) == 0) {
+            o.write((char *) &buff, sizeof(buff));
+            buff = 0;
+            buff_write(&buff, code, 18, 0);
+        }
+    }
+    if (bit_length == 8) {
+        //for 9 bit code 9th bit is 0
+        if (buff_write(&buff, code, 8, 0) == 0) {
+            o.write((char *) &buff, sizeof(buff));
+            buff = 0;
+            buff_write(&buff, code, 8, 0);
+        }
+    }
+}
+
+void Encode(char ipfile[], char opfile[], char dictionary[]) {
+    ifstream fin(ipfile, ios::binary);
+    ofstream o(opfile, ios::binary);
+    ifstream codefile(dictionary, ios::in);
+
+    unordered_map<string, int> table;
+    string present;
+    int i = 0;
+    while (!codefile.eof()) {
+        codefile >> present;
+        table[present] = i;
+        i++;
+    }
+
+    //encoding
+    string p;
+    char c;
+    unsigned char temp;
+    short int capital = 0;
+    int count = 0;
+    uint64_t buff = 0;
+
+    while (fin.get(c)) {
+        if (c == 0) {
+            if (table.find(p) != table.end()) {
+                count++;
+                prepare_buffer(buff, o, table[p], 18, capital);
+                //cout<<"Table "<<p<<" "<<table[p]<<endl;
+            } else if (p.length() >= 1) {
+                if (capital)p[0] = (char) toupper(p[0]);
+                for (int i = 0; i < p.length(); i++) {
+                    temp=int(p[i]);
+                    prepare_buffer(buff, o, temp, 8);
+                    //cout<<p[i]<<" "<<int(p[i])<<endl;
+                }
+            }
+            capital = 0;
+            p.clear();
+        }
+        else
+        {
+            if (p.length() == 0) {
+                if (isalpha(c) && isupper(c)) { //if first letter is uppercase convert
+                    capital = 1;
+                    p += (char) tolower(c);
+                } else {
+                    p += c;
+                }
+            } else {
+                p += c;
+            }
+        }
+    }
+
+    if (buff_write(&buff, 0, 0, 1) != 0)
+        o.write((char *) &buff, sizeof(buff));
+
+    cout << "number of words used from dictionary " << count << "\n";
+    fin.close();
+    o.close();
+    codefile.close();
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        cout << "Correct usage: ./a.out tokens opfile dictionary";
+        exit(0);
+    }
+    auto start = high_resolution_clock::now();
+    Encode(argv[1], argv[2], argv[3]);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by function: "<< duration.count() << " microseconds" << endl;
+}
